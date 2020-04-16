5f30e73e4d4ac599ec676a4048355022b9b2754f
ahavamorse <ahavamorse@icloud.com>
2020-04-16 09:31:54 -0700

    Added Collection View and cell to storyboard

---
+++ PhotoCollection/PhotoCollection/View/PhotosCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Ahava on 4/16/20.
+//  Copyright © 2020 Ahava. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoTitleLabel: UILabel!
+    
+}
