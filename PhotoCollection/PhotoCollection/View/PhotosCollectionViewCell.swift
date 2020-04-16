da970c3f30119cbc9b3a3d88b220a1ca284b1bcd
ahavamorse <ahavamorse@icloud.com>
2020-04-16 09:51:56 -0700

    Added photo collection view cell updateViews method

--- PhotoCollection/PhotoCollection/View/PhotosCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/View/PhotosCollectionViewCell.swift
@@ -13,4 +13,16 @@ class PhotosCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var photoTitleLabel: UILabel!
     
+    var photo: Photo? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    func updateViews() {
+        if let data = photo?.imageData {
+            photoImageView.image = UIImage(data: data)
+        }
+        photoTitleLabel.text = photo?.title
+    }
 }
